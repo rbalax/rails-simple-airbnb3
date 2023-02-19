2a6ff3c57adfaad0862aaa0ff2fa911427350cf4
Rom Balax <rbalax@yahoo.co.uk>
2023-02-20 08:46:13 +0900

    added flat rendering in colummmns plus form for new flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+puts "cleaning up dB"
+Flat.delete_all
+puts "creating flats seeding dB"
+
+10.times do
+  Flat.create(
+    name: Faker::Address.street_name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(50..500),
+    number_of_guests: rand(1..6)
+  )
+end
+
+puts "seeding dB is DONE"
