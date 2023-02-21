722a4c4ac269b3188affd4b44c8366a958a21a36
Rom Balax <rbalax@yahoo.co.uk>
2023-02-21 21:45:17 +0900

    finished show

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -10,4 +10,25 @@ class FlatsController < ApplicationController
   def new
     @flat = Flat.new
   end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
